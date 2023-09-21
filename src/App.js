9ee89bc810ab6cf877776abfee9285a7884f7887
Bharath K R <bharath.k@spurtreetech.in>
2023-09-22 01:23:17 +0530

    initial commit basecode setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <><Nav></Nav><div className="bg-indigo-600 w-full h-screen">
+    </div></>
+  );
+}
+
+export default App;
